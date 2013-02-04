09b66b7b583d9808113b5192d50200ccbd62f0ef
20m61 <ifm0s1m0s1m0nn@hotmail.com>
2013-02-04 18:36:15 +0900

    scroll実装

--- morph brace/morph brace/ViewController.m
+++ morph brace/morph brace/ViewController.m
@@ -15,79 +15,67 @@
 @end
 
 @implementation ViewController
+{
+    IBOutlet UIScrollView *scrollView;
+}
+
+const CGFloat kScrollObjHeight = 460.0;//(※1)1pageの高さ
+const CGFloat kScrollObjWidth  = 320.0;//(※2)1pageの幅
+const NSUInteger kNumImages    = 3;    //(※3)総page数
+
+//ScrollViewの設定
+- (void)layoutScrollImages
+{
+    UIImageView *view = nil;
+    NSArray *subviews = [scrollView subviews];
+    
+    CGFloat curXLoc = 0;
+    for (view in subviews)
+    {
+        if ([view isKindOfClass:[UIImageView class]] && view.tag > 0)
+        {
+            CGRect frame = view.frame;
+            frame.origin = CGPointMake(curXLoc, 0);
+            view.frame = frame;
+            
+            curXLoc += (kScrollObjWidth);
+        }
+    }
+    
+    //スクロールの総範囲
+    [scrollView setContentSize:CGSizeMake((kNumImages * kScrollObjWidth), kScrollObjHeight)];
+}
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
+    
+    NSUInteger i;
+    for (i = 1; i <= kNumImages; i++)
+    {
+        NSString *imageName = [NSString stringWithFormat:@"picture01.jpg"];
+        UIImage *image = [UIImage imageNamed:imageName];
+        UIImageView *imageView = [[UIImageView alloc] initWithImage:image];
+        
+        CGRect rect = imageView.frame;
+        rect.size.height = kScrollObjHeight;
+        rect.size.width = kScrollObjWidth;
+        imageView.frame = rect;
+        imageView.tag = i;
+        [scrollView addSubview:imageView];
+    }
+    
+    [self layoutScrollImages];
+    
     NSLog(@"prepare");
-    
-    
-    // UIself.scrollViewのインスタンスを生成
-    self.scrollView = [[UIScrollView alloc] initWithFrame:self.view.bounds];
-    
-    // UIself.scrollViewのDelegateにself（ViewController）を設定
-    self.scrollView.delegate = self;
-    
-    // コンテンツサイズをスクロールビューの表示領域より大きい値に設定する（値は適当）
-    self.scrollView.contentSize = CGSizeMake(2000.0f, 2000.0f);
-    
-    // ズームスケールの最大・最小値を設定
-    self.scrollView.maximumZoomScale = 2.0f;
-    self.scrollView.minimumZoomScale = 0.5f;
-    self.scrollView.maximumZoomScale = 2.0f;
-    self.scrollView.minimumZoomScale = 0.5f;
-    
-    // 背景色にスクロールビューテクスチャを設定した矩形を配置
-    UIView *aView = [[UIView alloc] initWithFrame:CGRectMake(0.0f, 0.0f, 2000.0f, 2000.0f)];
-    aView.backgroundColor = [UIColor scrollViewTexturedBackgroundColor];
-    [self.scrollView addSubview:aView];
-    
-    // UISwipeGestureRecognizerインスタンスを生成
-    // スワイプが実行されたときに- swiped:メソッドを実行する
-    UISwipeGestureRecognizer *swipeGestureRecognizer = [[UISwipeGestureRecognizer alloc]
-                                                        initWithTarget:self
-                                                        action:@selector(swiped:)];
-    
-    // 上方向のスワイプを検知する
-    swipeGestureRecognizer.direction = UISwipeGestureRecognizerDirectionUp;
-    
-    // スワイプが失敗するまでパン（ドラッグ）を実行しない！
-    [self.scrollView.panGestureRecognizer requireGestureRecognizerToFail:swipeGestureRecognizer];
-    
-    // スクロールビューにスワイプジェスチャーを登録
-    [self.scrollView addGestureRecognizer:swipeGestureRecognizer];
-    
-    // ２本指でスワイプされたときに動作するようにする
-    swipeGestureRecognizer.numberOfTouchesRequired = 2;
-    
-    // 上方向のスワイプを検知する
-    swipeGestureRecognizer.direction = UISwipeGestureRecognizerDirectionUp;
-    
-    // スクロールビューにスワイプジェスチャーを登録
-    [self.scrollView addGestureRecognizer:swipeGestureRecognizer];
-    
-    // ビューにself.scrollViewを追加
-    [self.view addSubview:self.scrollView];
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
-}
-
-- (UIView *)viewForZoomingInScrollView:(UIScrollView *)scrollView
-{
-    // - viewDidLoadメソッドで生成したaViewが返る
-    return [[self.scrollView subviews] objectAtIndex:0];
-}
-
-// スワイプジェスチャーを検知したときに呼ばれるメソッド
-- (void)swiped:(UISwipeGestureRecognizer *)sender
-{
-    // スワイプされたらログ出力
-    NSLog(@"swiped!!");
 }
 
 
