d4a29ad655cc647b10eee561e016cf752d5887b2
20m61 <ifm0s1m0s1m0nn@hotmail.com>
2013-02-14 18:47:14 +0900

    な〜んか動きが怪しい（beltToUsual界隈）

--- code/morph_blass/morph_blass/ViewController.m
+++ code/morph_blass/morph_blass/ViewController.m
@@ -10,6 +10,10 @@
 #import <UIKit/UIKit.h>
 #import <AVFoundation/AVFoundation.h>
 
+@interface UIView ()
+- (NSString *)recursiveDescription;
+@end
+
 @interface ViewController()
 //画面サイズを合わせるためのプロパティ
 @property float screenW;
@@ -24,6 +28,7 @@
 @property UIImageView *imageViewAnims1;
 @property UIImageView *imageViewAnims2;
 @property UIImageView *imageViewAnims3;
+@property UIImageView *imageViewAnims4;
 @end
 
 @implementation ViewController
@@ -34,14 +39,7 @@
 	// Do any additional setup after loading the view, typically from a nib.
     
     [self viewWillAppear:YES]; //prepare
-    [self prepareUsualView]; //usual
-    [self prepareMophing]; //mophing
-    [self prepareBeltView]; //beltView
-    [self prepareBeltTouchView]; //beltTouchView
-    [self prepareIvents]; //ivents
-    
-    self.beltView.alpha = .0f; //beltViewを不可視化
-    NSLog(@"veltView invisible");
+    [self resetToUsualView];
 }
 
 #pragma mark -
@@ -59,13 +57,55 @@
     NSLog(@"viewWillAppear: %@", NSStringFromCGRect(self.screenSizeRect));
 }
 
+- (void)resetToUsualView{
+    [self prepareUsualView]; //usual
+    [self prepareMophing]; //mophing
+    [self prepareBeltView]; //beltView
+    [self prepareBeltTouchButton]; //beltTouchView
+    [self prepareIvents]; //ivents
+    
+    self.beltView.alpha = 0.0f; //beltViewを不可視化
+    NSLog(@"veltView invisible");
+    self.beltTouchButton.alpha = .0f; //beltTouchButtonを不可視化
+    NSLog(@"veltTouchButton invisible");
+    self.imageViewAnims1.alpha = .0f; //imageViewAnims1を不可視化
+    NSLog(@"imageViewAnims1 invisible");
+    self.imageViewAnims2.alpha = .0f; //imageViewAnims2を不可視化
+    NSLog(@"imageViewAnims2 invisible");
+    self.imageViewAnims3.alpha = .0f; //imageViewAnims3を不可視化
+    NSLog(@"imageViewAnims3 invisible");
+    
+    NSLog(@"\n beltView.alpha: %f \n beltTouchButton.alpha: %f", self.beltView.alpha, self.beltTouchButton.alpha);
+}
+
+- (void)reset{
+    
+    self.usualScrollView.alpha = 1.f; //beltViewを不可視化
+    NSLog(@"usualScrollView  %@", self.usualScrollView);
+    self.beltView.alpha = .0f; //beltViewを不可視化
+    NSLog(@"veltView %@", self.beltView);
+    self.beltTouchButton.alpha = .0f; //beltTouchButtonを不可視化
+    NSLog(@"veltTouchButton %@", self.beltTouchButton);
+    self.imageViewAnims1.alpha = .0f; //imageViewAnims1を不可視化
+    NSLog(@"imageViewAnims1 %@", self.imageViewAnims1);
+    self.imageViewAnims2.alpha = .0f; //imageViewAnims2を不可視化
+    NSLog(@"imageViewAnims2 %@", self.imageViewAnims2);
+    self.imageViewAnims3.alpha = .0f; //imageViewAnims3を不可視化
+    NSLog(@"imageViewAnims3 %@", self.imageViewAnims3);
+    self.imageViewAnims4.alpha = .0f; //imageViewAnims3を不可視化
+    NSLog(@"imageViewAnims4 %@", self.imageViewAnims4);
+
+    
+    NSLog(@"\n beltView.alpha: %f \n beltTouchButton.alpha: %f", self.beltView.alpha, self.beltTouchButton.alpha);
+}
+
 #pragma mark -
 #pragma mark === usual  ===
 #pragma mark
 - (void)prepareUsualView{
     // UIScrollViewのインスタンス化--------------------------------------------------
     self.usualScrollView = [[UIScrollView alloc]initWithFrame:self.screenSizeRect];
-    NSLog(@"scrollView: %@", NSStringFromCGRect(self.usualScrollView.frame));
+    NSLog(@"usualScrollView: %@", NSStringFromCGRect(self.usualScrollView.frame));
     // UIScrollViewのオプション設定
     self.usualScrollView.bounces = YES;
     self.usualScrollView.showsHorizontalScrollIndicator = NO;
@@ -74,13 +114,13 @@
     // UIImageView(scrollViewにのるimageView)のインスタンス化-------------------------
     CGRect usualImageViewSizeRect = CGRectMake(0, 0, self.screenH/2*3, self.screenW);
     UIImageView *usualImageView = [[UIImageView alloc]initWithFrame:usualImageViewSizeRect];
-    NSLog(@"imageView: %@", NSStringFromCGRect(usualImageView.frame));
+    NSLog(@"usualImageView: %@", NSStringFromCGRect(usualImageView.frame));
     // 画像を設定
     usualImageView.image = [UIImage imageNamed:@"UIUsual.png"];
     
     // scrollViewにimageViewを追加-------------------------------------------------
     [self.usualScrollView addSubview:usualImageView];
-    NSLog(@"add imageView on usualScrollView");
+    NSLog(@"add usualImageView on usualScrollView");
     // scrollViewのコンテンツサイズをimageViewに合わせる
     self.usualScrollView.contentSize = usualImageView.bounds.size;
     
@@ -100,36 +140,41 @@
 - (void)prepareBeltView{
     // UIView(beltView)をインスタンス化----------------------------------------------
     self.beltView = [[UIView alloc]initWithFrame:self.screenSizeRect];
-    NSLog(@"scrollView: %@", NSStringFromCGRect(self.usualScrollView.frame));
+    NSLog(@"beltView: %@", NSStringFromCGRect(self.beltView.frame));
     
     // UIImageView(beltViewにのるimageView)のインスタンス化---------------------------
     UIImageView *beltImageView = [[UIImageView alloc]initWithFrame:self.screenSizeRect];
-    NSLog(@"imageViewBelt: %@", NSStringFromCGRect(beltImageView.frame));
+    NSLog(@"beltImageView: %@", NSStringFromCGRect(beltImageView.frame));
     // 画像を設定
     beltImageView.image = [UIImage imageNamed:@"UIBelt.png"];
     
     // UIViewにimageViewを追加------------------------------------------------------
     [self.beltView addSubview:beltImageView];
-    NSLog(@"add imageView on beltView");
+    NSLog(@"add beltImageView on beltView");
     
     // viewをbeltViewに追加---------------------------------------------------------
     [self.view addSubview:self.beltView];
     NSLog(@"visible veltView");
-    
-//    [self prepareTouchIventBeltToUsual];
 }
 
 //ベルトモードのON/OFFを操作するタッチエリア
-- (void)prepareBeltTouchView{
+- (void)prepareBeltTouchButton{
+    //beltTouchButtonの大きさを定義する四角形
     int H = self.screenH;
     CGRect box = CGRectMake((H-100), 0, 100, 100);
     
+    //beltTouchButtonをインスタンス化
     self.beltTouchButton = [[UIButton alloc]initWithFrame:box];
-    [self.beltTouchButton addTarget:self action:@selector(touchIventUsualToBelt:) forControlEvents:UIControlEventTouchDown];
+    NSLog(@"beltTouchButton: %@", NSStringFromCGRect(self.beltTouchButton.frame));
     
+    //beltTouchButtonが押されたときtouchIventUsualToBelt:を実行する
+    [self.beltTouchButton addTarget:self action:@selector(touchIventUsualToBelt:) forControlEvents:UIControlEventTouchDown];
+    //beltTouchButtonの範囲外でタッチを解除したときtouchIventBeltToUsual:を実行する
     [self.beltTouchButton addTarget:self action:@selector(touchIventBeltToUsual:) forControlEvents:UIControlEventTouchUpOutside];
     
+    //viewへbeltTouchButtonを追加
     [self.view addSubview:self.beltTouchButton];
+    NSLog(@"add beltTouchButton on view");
 }
 
 #pragma mark -belt mode ivents//->touchIvents
@@ -180,46 +225,164 @@
     //設定
     self.imageViewAnims3.animationDuration = 0;
     self.imageViewAnims3.animationRepeatCount = 1;
+    
+    
+    //anims4をanimateするviewをインスタンス化-----------------------------------------
+    self.imageViewAnims4 = [[UIImageView alloc]initWithFrame:self.screenSizeRect];
+    NSMutableArray *anims4 = [[NSMutableArray alloc]init];
+    for (int i = 0; i < 59; i++) {
+        UIImage *tempImgAnims4 = [UIImage imageNamed:[NSString stringWithFormat:@"anims1_%03d.png",i]];
+        [anims4 addObject:tempImgAnims4];
+    }
+    self.imageViewAnims4.animationImages = anims4;
+    [self.view addSubview:self.imageViewAnims4];
+    NSLog(@"anims3-00->%@", [self.imageViewAnims4.animationImages objectAtIndex:0]);
+    //設定
+    self.imageViewAnims4.animationDuration = 0;
+    self.imageViewAnims4.animationRepeatCount = 1;
 }
 
-//usualからbeltへ
-- (void)morphingUsualToBelt
+#pragma mark -play & stop methods
+
+//animsの再生・停止メソッド
+//anims1を可視化して再生-------------------------------
+- (void)playAnims1{
+self.imageViewAnims1.alpha = 1.0f;
+[self.imageViewAnims1 startAnimating];
+NSLog(@"play anims1");
+}
+//anims1を停止して不可視化
+- (void)stopAnims1{
+[self.imageViewAnims1 stopAnimating];
+self.imageViewAnims1.alpha = .0f;
+NSLog(@"stop anims1");
+}
+//anims2を可視化して再生-------------------------------
+- (void)playAnims2{
+    self.imageViewAnims2.alpha = 1.0f;
+    [self.imageViewAnims2 startAnimating];
+    NSLog(@"play anims2");
+}
+//anims2を停止して不可視化
+- (void)stopAnims2{
+    [self.imageViewAnims2 stopAnimating];
+    self.imageViewAnims2.alpha = .0f;
+    NSLog(@"stop anims2");
+}
+//anims3を可視化して再生-------------------------------
+- (void)playAnims3{
+    self.imageViewAnims3.alpha = 1.0f;
+    [self.imageViewAnims3 startAnimating];
+    NSLog(@"play anims3");
+}
+//anims3を停止して不可視化
+- (void)stopAnims3{
+    [self.imageViewAnims3 stopAnimating];
+    self.imageViewAnims3.alpha = .0f;
+    NSLog(@"stop anims3");
+}
+//anims4を可視化して再生-------------------------------
+- (void)playAnims4{
+    self.imageViewAnims4.alpha = 1.0f;
+    [self.imageViewAnims4 startAnimating];
+    NSLog(@"play anims4");
+}
+//anims4を停止して不可視化
+- (void)stopAnims4{
+    [self.imageViewAnims4 stopAnimating];
+    self.imageViewAnims4.alpha = .0f;
+    NSLog(@"stop anims4");
+}
+
+#pragma mark -morphing meyhods
+//usualからmorphingへ
+- (void)morphingUsualToBeltBeforeTouch
 {
-    [UIView animateWithDuration:3.0f
+    //scrollViewを不可視化
+    self.usualScrollView.alpha = .0f;
+    NSLog(@"usualScrollView invisible");
+    
+    //animate
+    [UIView animateWithDuration:2.f
                           delay:0.0f
                         options:UIViewAnimationOptionAllowUserInteraction
                      animations:^(void)
      {
-         NSLog(@"startAnimation");
-         [self.imageViewAnims1 startAnimating];
-         NSLog(@"anims1");
+         //anims1を可視化して再生
+         [self playAnims1];
      }
                      completion:^(BOOL finished)
      {
-         //scrollViewを透明に
-         self.usualScrollView.alpha = .0f;
+         //anims1を停止して不可視化
+         [self stopAnims1];
          
-         [UIView animateWithDuration:3.0f
-                               delay:4.0f
-                             options:UIViewAnimationOptionAllowUserInteraction
+         [UIView animateWithDuration:.0f
                           animations:^(void)
           {
-              [self.imageViewAnims2 startAnimating];
-              NSLog(@"anims2");
+              //anims2を可視化して再生
+              [self playAnims2];
+              
+              self.beltTouchButton.alpha = 1.0;
           }
                           completion:^(BOOL finished){}
           ];
      }
      ];
+}
+
+//morphingからbeltへ
+- (void)morphingUsualToBeltAfterTouch
+{
+    //anims2を停止して不可視化
+    [self stopAnims2];
     
-    NSLog(@"morphing!: usual to belt");
+    //animate
+    [UIView animateWithDuration:.5f
+                     animations:^(void)
+     {
+         //anims3を可視化して再生
+         [self playAnims3];
+     }
+                     completion:^(BOOL finished)
+     {
+         //beltViewを可視化
+         self.beltView.alpha = 1.0f;
+         NSLog(@"beltView visible");
+         
+         //anims3を停止して不可視化
+         [self stopAnims3];
+     }
+     ];
 }
 
 //beltからusualへ
 - (void)morphingBeltToUsual
 {
     
-    NSLog(@"morphing!: belt to usual");
+    
+    //beltViewを不可視化
+    self.beltView.alpha = .0f;
+    //animate
+    [UIView animateWithDuration:2.05f
+                     animations:^(void)
+     {
+         //beltViewを不可視化
+         self.beltView.alpha = .0f;
+
+         //anims1を可視化して再生
+         [self playAnims4];
+     }
+                     completion:^(BOOL finished)
+     {
+         //beltViewを不可視化
+         self.beltView.alpha = .0f;
+         
+        //anims1を停止して不可視化
+        [self stopAnims1];
+        
+        [self reset];
+        NSLog(@"resetToUsualView");
+    }];
 }
 
 #pragma mark -morphing ivents//->touchIvents
@@ -239,6 +402,7 @@
 }
 
 //scrollViewがスクロールされているときに実行
+#if DETECT_SCROLL
 -(void)scrollViewDidScroll:(UIScrollView *)scrollView
 {
     //scrollViewのコンテンツ位置(scrollViewの中央でみている)
@@ -246,75 +410,42 @@
     float y = scrollView.contentOffset.y;
     NSLog(@"scrolling: %f, %f", x, y);
 }
+#endif
 
 //scrollViewがスクロールされ終わったときに実行
 - (void)scrollViewDidEndDecelerating:(UIScrollView *)scrollView
 {
-    //scrollViewのコンテンツ位置(scrollViewの中央でみている)
+    //scrollViewのコンテンツ位置(scrollViewのx軸の中央でみている)
     float x = scrollView.contentOffset.x;
     float y = scrollView.contentOffset.y;
     NSLog(@"scrolled: %f, %f", x, y);
     
     //scrollViewのコンテンツ位置が画面の中央に来たときに実行
     if (x == (self.screenH/2)) {
-        [self morphingUsualToBelt];
-        
-        //beltTouchViewを可視化
-        NSLog(@"beltTouchView visible");
-        
-        //暫定
-//        [self prepareTouchIventUsualToBelt];
+        [self morphingUsualToBeltBeforeTouch];
+        NSLog(@"called morphingUsualToBeltBeforeTouch");
     }
 }
 
 #pragma mark -touchIvents
 - (void)prepareTouchIvents{
-//    [self prepareTouchIventUsualToBelt];
-//    [self prepareTouchIventBeltToUsual];
-}
-
-//longTouchをトリガーにしたイベント
-- (void)prepareTouchIventUsualToBelt{
     
 }
 
-//mophingUsualToBelt
+//morphingUsualToBelt実行中にbeltTouchButtonにタッチした瞬間に実行
 - (void)touchIventUsualToBelt:(UIButton *)beltTouchButton {
-    NSLog(@"belt touch!");
-    
-    //anims2を透明にしてanims3を実行->beltViewの準備
-    [UIView animateWithDuration:.5f
-                          delay:0.0f
-                        options:UIViewAnimationOptionAllowUserInteraction
-                     animations:^(void)
-     {
-         self.imageViewAnims2.alpha = .0f;
-         [self.imageViewAnims3 startAnimating];
-         NSLog(@"anims2");
-     }
-                     completion:^(BOOL finished)
-     {
-         self.imageViewAnims3.alpha = .0f;
-         self.beltView.alpha = 1.0f;
-         NSLog(@"beltView visible");
-     }
-     ];
+    NSLog(@"belt touch up!");
+    [self morphingUsualToBeltAfterTouch];
+    NSLog(@"called morphingUsualToBeltAfterTouch");
 }
 
-
-//右スワイプをトリガーにしたイベント
--(void)prepareTouchIventBeltToUsual{
-
-}
-
+//beltTouchButtonの外でタッチを解除したときに実行
 - (void)touchIventBeltToUsual:(UIButton *)beltTouchButton {
-    NSLog(@"swipe right");
+    NSLog(@"belt touch down outside!");
     
-    //beltVoiewを透明に
-    self.beltView.alpha = .0f;
-
-    [self prepareUsualView];
-    [self prepareMophing];
+    [self morphingBeltToUsual];
+    NSLog(@"%@", [self.view recursiveDescription]);
+    
 }
 
 - (void)didReceiveMemoryWarning
